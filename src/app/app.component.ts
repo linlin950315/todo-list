49df3f7e5396f3ee1442e0aaeec3ee9683af9780
zhangll <zhangll@tigersoft.co.jp>
2024-12-11 18:28:24 +0900

    1211

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
+import { CommonModule } from "@angular/common";
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [],
+  standalone: true,  //描述组件是否需要NgModule
+  //当应用为独立应用时，它将使用导入直接管理模板依赖项（组件、指令等）。
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title1 = 'To do application';
-  subtitle = 'This is a simple to do application'
+  subtitle = 'This is a simple to do application';
+  componentTitle = "componentTitle = My To Do List";
+//boolean表达式
+  filter: "all" | "active" | "done" = "all";
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+    { description: "study" },
+  ];
+
+  get items() {
+    if (this.filter === "active") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string) {
+    if (!description) return;
+
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
 }
+
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './item.component.html',
+  styleUrl: './item.component.css'
+})
+export class ItemComponent {
+
+}
+
+
+//https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Angular_item_component
